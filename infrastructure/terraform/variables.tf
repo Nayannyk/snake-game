39fe655d9f586d9fdab703abcd12355fc81c6be1
Ubuntu <ubuntu@ip-172-31-9-96.ap-south-1.compute.internal>
2026-07-06 10:42:28 +0000

    fix: default instance_type to t2.medium

--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -13,7 +13,7 @@ variable "environment" {
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-    default     = "t2.micro"
+  default     = "t2.medium"
 }
 
 variable "root_volume_size" {
